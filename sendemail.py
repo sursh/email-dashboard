e9d1fe7e06639ec62a4d2109d8637ec875d8bf03
Sasha Laundy <sasha.laundy@gmail.com>
2013-08-06 18:47:52 -0400

    Update script with new API changes

--- sendemail.py
+++ sendemail.py
@@ -11,24 +11,37 @@
 
 import os
 import requests
+import json
 from werkzeug import MultiDict as MultiDict
 from config_mailgun import MAILGUN_KEY, MAILGUN_FROM, MAILGUN_TO
 
 POST_URL = 'https://api.mailgun.net/v2/refugeesunited.mailgun.org/messages'
-
+DATAFILE = 'data/new.json'
 PICDIR = 'temp'
 
-def send_inline_image(imagelist):
-    files = MultiDict()
-    html = ['<html>']
-    i = 1
-    # this 'i' business is jank to work around a Multidict bug in Requests:
-    # https://github.com/kennethreitz/requests/issues/1155
-    for image in imagelist:
-        files.add('inline[' + str(i) + ']', open(os.path.join(PICDIR, image)))
-        html.append('<p>Inline image here: <img src="cid:%s" alt="%s"></p>' % (image, image)) #TODO make this alt text more readable
-        i += 1
-    html.append('</html>')
+def send_email():
+    with open(DATAFILE, 'r') as f:
+        wholefile = f.read()
+        wholefile = json.loads(wholefile)
+        # wholefile is now a list, each element being a dict for one graph
+
+        files = MultiDict()
+        html = ['<html>']
+        # this 'i' business is jank to work around a Multidict bug in Requests:
+        # https://github.com/kennethreitz/requests/issues/1155
+        i = 1
+        for graph in wholefile:
+            if graph['type'] == 'table':
+                pass
+            elif graph['type'] == 'timeseries':
+                pass
+            elif graph['type'] == 'bar' and graph['data']:
+                imagename = graph['uniquename'] + '.png'
+                files.add('inline[' + str(i) + ']', open(os.path.join(PICDIR, imagename)))
+                i += 1
+                html.append('<p>%s %s</p> <p><img src="cid:%s" alt="%s"></p>' % (graph['name'].title(), graph['additional'].title(), imagename, graph['name']+' graph'))
+
+        html.append('</html>')
 
     return requests.post(
         POST_URL,
@@ -36,17 +49,12 @@ def send_inline_image(imagelist):
         files=files,
         data={"from": MAILGUN_FROM,
               "to": MAILGUN_TO,
-              "subject": "Daily Status Report",
+              "subject": "Daily Status Report yeahhhh",
               "text": "Daily snapshot of the Refugees United dashboard.",
               "html": ''.join(html),
               })
 
 if __name__ == '__main__':
 
-    imagelist = []
-    for f in os.listdir(PICDIR):
-        if f.endswith('.png'):
-            imagelist.append(f)
-
-    r = send_inline_image(imagelist)
+    r = send_email()
     print r.text
